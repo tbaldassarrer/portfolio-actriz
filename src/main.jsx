4aa76b825b2e9d3accd0117f8bfbb182e3d3ea08
tbaldassarrer <tbaldassarrer@gmail.com>
2026-03-01 18:40:15 +0000

    Fix: mount App and set Vite base for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./styles.css";
 
-
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
